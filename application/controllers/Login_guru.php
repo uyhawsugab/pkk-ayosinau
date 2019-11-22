df95bbe34d21ac06e48a467a50c5b96e6e4bb364
fanirahmat <fanirahmatulloh842@gmail.com>
2019-11-23 00:37:50 +0700

    menambah assets template user

--- application/controllers/Login_guru.php
+++ application/controllers/Login_guru.php
@@ -7,6 +7,43 @@ class Login_guru extends CI_Controller {
 	{
 		$this->load->view('v_login_guru');
 	}
+	public function login()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$cek = $this->db->get_where('guru', ['username' => $username])->row_array();
+		if ($cek)
+		{
+			if ($password == $cek['password']) 
+			{
+				$data = [
+					'username' => $cek['username'],
+					'nama_guru' => $cek['nama_guru'],
+					'id_level' => $cek['id_level'],
+					'logged' => true
+				];
+				$this->session->set_userdata($data);
+				$dt['status']=1;
+				echo json_encode($dt);
+			} 
+			else 
+			{
+				$dt['status']=0;
+				echo json_encode($dt);
+			}
+		} 
+		else 
+		{
+			$dt['status']=0;
+			echo json_encode($dt);
+		}
+		
+	}
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect('Login_guru','refresh');
+	}
 
 }
 
