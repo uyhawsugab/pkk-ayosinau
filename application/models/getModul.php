35ff25745ba85960cf600f376f56a69aa2b3718f
zReikatsu <wahyu_bagus_26rpl@student.smktelkom-mlg.sch.id>
2019-11-26 18:30:42 +0700

    Buy modul from user side

---
+++ application/models/getModul.php
@@ -0,0 +1,43 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class getModul extends CI_Model {
+
+	public function getModul()
+	{
+		$modul = $this->db->get('modul')->result();
+		return $modul;
+	}
+
+	public function searchModul($nama)
+	{
+		$result = $this->db->like('nama_modul', $nama)->get('modul')->result();
+		return $result;
+	}
+
+	public function getDetailData($id)
+	{
+		$result = $this->db
+		->join('jenis_modul', 'jenis_modul.id_jenis_modul = modul.id_jenis_modul')
+		->join('mapel', 'mapel.id_mapel = modul.id_mapel')
+		->where('id_modul', $id)
+		->get('modul')->row();
+
+		return $result;
+	}
+
+	public function createNota()
+	{
+		$data = array(
+			'id_user' => $this->session->userdata('id_user'),
+			'tanggal' => date('Y-m-d')
+		);
+
+		return $this->db->insert('nota', $data);
+	}
+
+}
+
+/* End of file getModul.php */
