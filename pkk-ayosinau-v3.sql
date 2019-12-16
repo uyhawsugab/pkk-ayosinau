ca30977d98fd7d360c5f9e73cf2e6d7881fd457b
zReikatsu <wahyu_bagus_26rpl@student.smktelkom-mlg.sch.id>
2019-12-16 08:25:52 +0700

    Add new Database

--- pkk-ayosinau-v3.sql
+++ pkk-ayosinau-v3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Nov 2019 pada 14.41
+-- Waktu pembuatan: 16 Des 2019 pada 02.24
 -- Versi server: 10.1.37-MariaDB
 -- Versi PHP: 7.3.1
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pkk-ayosinau-v2`
+-- Database: `pkk-ayosinau-v3`
 --
 
 -- --------------------------------------------------------
@@ -80,7 +80,8 @@ INSERT INTO `mapel` (`id_mapel`, `nama_mapel`) VALUES
 (1, 'Matematika'),
 (2, 'Bahasa Indonesia'),
 (3, 'Bahasa Inggris'),
-(4, 'Lain - Lain');
+(4, 'Pemrograman'),
+(5, 'PAI');
 
 -- --------------------------------------------------------
 
@@ -92,9 +93,9 @@ CREATE TABLE `modul` (
   `id_modul` int(11) NOT NULL,
   `nama_modul` varchar(255) NOT NULL,
   `harga` varchar(255) NOT NULL,
+  `link_modul` varchar(100) NOT NULL,
+  `deskripsi` text NOT NULL,
   `gambar` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `stok` int(11) NOT NULL,
   `id_jenis_modul` int(11) NOT NULL,
   `id_mapel` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -103,8 +104,29 @@ CREATE TABLE `modul` (
 -- Dumping data untuk tabel `modul`
 --
 
-INSERT INTO `modul` (`id_modul`, `nama_modul`, `harga`, `gambar`, `status`, `stok`, `id_jenis_modul`, `id_mapel`) VALUES
-(2, 'matrikd', '', '', '', 0, 2, 1);
+INSERT INTO `modul` (`id_modul`, `nama_modul`, `harga`, `link_modul`, `deskripsi`, `gambar`, `id_jenis_modul`, `id_mapel`) VALUES
+(11, 'HTML 5', 'gratis', 'http://html5-modul/723gada', 'Pengenalan dan Fundamental dasar tentang HTML 5', 'icon1.jpg', 2, 4),
+(12, 'Codeigniter 3.33', 'gratis', 'http://CI33-modul/757gada', 'Pengenalan dan fundamental dasar Codeigniter 3.3', 'icon2.jpg', 2, 4),
+(15, 'Laravel 6 Full', '125000', 'http://Laravel6full-modul/757gada', 'Memmbuat Aplikasi Toko Online Ok', 'icon3.png', 1, 4),
+(16, 'Android Zero To Hero', '220000', 'https://drive.google.com/file/d/1l7MgFO0FGaL4OUY2igkyXUCIYKtr-Cvw/view', 'Sebuah Modul yang berisikan panduan-panduan tentang memulai android studio', 'icon4.jpg', 1, 4),
+(17, 'Matematika XII 1', '20000', 'https://drive.google.com/file/d/1gWPoIxy1rGTG-NP9sgKVVZCZpYWPZ44d/view', 'Buku Matematika untuk kelas 12', 'icon8.jpg', 1, 1),
+(18, 'Pendidikan Agama Islam XII 1', '20000', 'https://classroom.google.com/u/1/c/Njk3NDMwOTY0NFpa', 'Buku Pendidikan Agama Islam untuk Kelas XII', 'pai1.jpg', 1, 5),
+(22, 'Bahasa Indonesia XII 1', 'gratis', 'https://classroom.google.com/u/1/c/NDA4ODI5MjM2OTNa/a/NDA5MzU2MDk2NDha/details', 'Modul awal kelas XII untuk pelajaran bahasa indonesia', 'icon981.jpg', 2, 2),
+(23, 'Bahasa Inggris XII 1', 'gratis', 'https://drive.google.com/file/d/1nEaTynS86aHCGmNFe2UWPYt_hVqTINrp/view', 'Modul pertama kelas 12 untuk pelajaran bahasa inggris', 'icon991.jpg', 2, 3),
+(24, 'Desain Grafis 1', 'gratis', 'https://drive.google.com/file/d/1l7MgFO0FGaL4OUY2igkyXUCIYKtr-Cvw/view', 'A', 'Use_Case_Diagram1.png', 2, 4),
+(25, 'Bahasa Indonesia XII 2', '125000', 'https://drive.google.com/file/d/1-5BhUwnthQetrl2dKbJGyRXPrKzcVQh_/view', 'A', 'icon982.jpg', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `my_modul`
+--
+
+CREATE TABLE `my_modul` (
+  `id_storage` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_modul` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -116,9 +138,33 @@ CREATE TABLE `nota` (
   `id_nota` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `metode` varchar(255) DEFAULT NULL,
   `bukti` varchar(255) NOT NULL,
   `grandtotal` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `nota`
+--
+
+INSERT INTO `nota` (`id_nota`, `id_user`, `tanggal`, `metode`, `bukti`, `grandtotal`) VALUES
+(1, 1, '2019-12-01 02:00:00', NULL, 'gatau', '125000'),
+(2, 1, '2019-12-02 08:48:05', 'BRI', '18.png', '0'),
+(3, 1, '2019-12-02 08:49:31', 'BNI', '181.png', '250000'),
+(4, 1, '2019-12-01 17:00:00', NULL, '', ''),
+(5, 3, '2019-12-02 10:53:41', 'BRI', '0164ef3201589650268ea9e2495a62d3.jpg', '2430000'),
+(6, 4, '2019-12-02 11:03:58', 'BNI', 'icon4.jpg', '250000'),
+(7, 1, '2019-12-03 01:02:22', 'BRI', 'ae.jpg', '18500000'),
+(8, 1, '2019-12-12 01:48:49', NULL, '', '240000'),
+(9, 1, '2019-12-12 01:51:06', 'BNI', 'CCF07042018_0006_page-0001.jpg', '240000'),
+(10, 1, '2019-12-11 17:00:00', NULL, '', ''),
+(11, 1, '2019-12-11 17:00:00', NULL, '', ''),
+(12, 1, '2019-12-11 17:00:00', NULL, '', ''),
+(13, 1, '2019-12-11 17:00:00', NULL, '', ''),
+(14, 1, '2019-12-12 04:03:09', 'BRI', 'CCE07042018_0003_page-0001.jpg', '345000'),
+(15, 1, '2019-12-12 04:04:12', 'BRI', 'CCF07042018_0005_page-0001.jpg', '240000'),
+(16, 7, '2019-12-12 04:35:17', 'BRI', 'CCF07042018_0005_page-00011.jpg', '145000'),
+(17, 1, '2019-12-13 05:49:19', 'BRI', 'CCE07042018_page-0001.jpg', '125000');
 
 -- --------------------------------------------------------
 
@@ -132,6 +178,32 @@ CREATE TABLE `transaksi` (
   `id_modul` int(11) NOT NULL,
   `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_nota`, `id_modul`, `qty`) VALUES
+(1, 2, 11, 1),
+(2, 3, 15, 2),
+(3, 5, 15, 2),
+(4, 5, 16, 8),
+(5, 5, 17, 21),
+(6, 6, 15, 2),
+(7, 7, 25, 100),
+(8, 7, 18, 300),
+(9, 8, 16, 1),
+(10, 8, 17, 1),
+(11, 9, 16, 1),
+(12, 9, 17, 1),
+(13, 14, 15, 1),
+(14, 14, 16, 1),
+(15, 15, 16, 1),
+(16, 15, 17, 1),
+(17, 16, 12, 1),
+(18, 16, 15, 1),
+(19, 16, 17, 1),
+(20, 17, 15, 1);
 
 -- --------------------------------------------------------
 
@@ -158,7 +230,10 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `nama`, `alamat`, `notelp`, `id_mapel`, `id_level`) VALUES
 (1, 'wahyu1', 'cdc49b1a4b4aa35af0bf2a7b20e6cc2f', 'wahyu@mail.com', 'wahyu bagus wicaksono', 'Malang', '087822128829', NULL, 1),
 (3, 'guru1', '92afb435ceb16630e9827f54330c59c9', 'guru1@mail.com', 'Pak Arifin', 'Malang', '086419282092', 4, 2),
-(4, 'admin1', 'e00cf25ad42683b3df678c61f42c6bda', 'admin1@mail.com', 'Admin', 'Malang', '08282839202', NULL, 3);
+(4, 'admin1', 'e00cf25ad42683b3df678c61f42c6bda', 'admin1@mail.com', 'Admin', 'Malang', '08282839202', NULL, 3),
+(5, 'fasya', '202cb962ac59075b964b07152d234b70', 'aaaa@gmail.com', 'aku', 'ss', '0328428342', NULL, 1),
+(6, 'sindu', '202cb962ac59075b964b07152d234b70', 'sindu22@gmail.com', 'Fauzi Dimas', 'Malang OK', '0823174298141', 5, 2),
+(7, 'irgiarya', 'a1a590abc8054a243f11d8ce9f4efab8', 'irgiarya.depok@gmail.com', 'Irgi Arya', 'Condet', '0878-2394-2392', NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -189,6 +264,14 @@ ALTER TABLE `modul`
   ADD PRIMARY KEY (`id_modul`),
   ADD KEY `id_jenis_modul` (`id_jenis_modul`),
   ADD KEY `id_mapel` (`id_mapel`);
+
+--
+-- Indeks untuk tabel `my_modul`
+--
+ALTER TABLE `my_modul`
+  ADD PRIMARY KEY (`id_storage`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_modul` (`id_modul`);
 
 --
 -- Indeks untuk tabel `nota`
@@ -233,31 +316,37 @@ ALTER TABLE `level`
 -- AUTO_INCREMENT untuk tabel `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `modul`
 --
 ALTER TABLE `modul`
-  MODIFY `id_modul` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_modul` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT untuk tabel `my_modul`
+--
+ALTER TABLE `my_modul`
+  MODIFY `id_storage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `nota`
 --
 ALTER TABLE `nota`
-  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -269,6 +358,13 @@ ALTER TABLE `user`
 ALTER TABLE `modul`
   ADD CONSTRAINT `modul_ibfk_1` FOREIGN KEY (`id_jenis_modul`) REFERENCES `jenis_modul` (`id_jenis_modul`),
   ADD CONSTRAINT `modul_ibfk_2` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`id_mapel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `my_modul`
+--
+ALTER TABLE `my_modul`
+  ADD CONSTRAINT `my_modul_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `my_modul_ibfk_2` FOREIGN KEY (`id_modul`) REFERENCES `modul` (`id_modul`);
 
 --
 -- Ketidakleluasaan untuk tabel `nota`
